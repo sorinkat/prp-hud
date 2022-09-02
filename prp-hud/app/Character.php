995d1b009d03f7c653e68c37e6fe568e7e2cc73e
sorinkat <sorinkat@gmail.com>
2022-09-02 17:51:27 -0600

    hud fetch

--- prp-hud/app/Character.php
+++ prp-hud/app/Character.php
@@ -12,13 +12,19 @@ class Character extends Model
         'hudid', 'name', 'commandchannel','combatanimations','socialanimations','rpanimations'
     ];
 
-    protected $appends = ['titlers'];
+    protected $appends = ['titlers','activetitler'];
 
 
     public function getTitlersAttribute()
     {
         $titlers = $this->hasMany(Titler::class,'character','id')->get();  
         return $titlers;
+    }
+
+    public function getActiveTitlerAttribute()
+    {
+        $titler = $this->hasOne(Titler::class,'character','id')->where('active',1)->first();  
+        return $titler;        
     }
 
     protected $hidden = [];
