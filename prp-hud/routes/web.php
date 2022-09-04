3cb9faa1f6d9d20407deb98d5897b59db2328f0a
sorinkat <sorinkat@gmail.com>
2022-09-03 22:30:21 -0600

    test

--- prp-hud/routes/web.php
+++ prp-hud/routes/web.php
@@ -38,7 +38,7 @@ $router->group(['prefix'=>'api'], function () use ($router) {
     $router->put('titler/{id}', ['uses' => 'TitlerController@update']);
 
     // Enables the selected hud and disables all others.  Pass in 0 to disable all huds for character.
-    $router->put('titler/{cid}/enable', ['uses' => 'TitlerController@changeState']);  
+    $router->put('titler/{cid}/enable', ['uses' => 'CharacterController@changeState']);  
     $router->put('titler/{cid}/disable', ['uses' => 'CharacterController@enableTitler']);    
     $router->put('titler/{cid}/{hud}', ['uses' => 'CharacterController@disableTitler']);
            
